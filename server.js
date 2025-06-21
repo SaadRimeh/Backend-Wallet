bac4bac2824fef337b141a2232cdcb492ea53b54
Saad Rimeh <saad.rimeh.2001.01@gmail.com>
2025-06-21 19:17:30 +0300

    cron.js

--- server.js
+++ server.js
@@ -2,25 +2,26 @@ import express from 'express';
 import dotenv from "dotenv";
 import { intDB } from './config/db.js';
 import transactionsRoute from "./routes/transactionsRoute.js"
+import job from './config/cron.js';
+
 
 dotenv.config();
 
 const app = express();
+
+ if(process.env.NODE_ENV==="production")  job.start();
+
 app.use(express.json());
 
-
 const PORT = process.env.PORT || 5001;
-
-
-
-
-app.use("/api/transactions",transactionsRoute);
-
-intDB().then(()=>{
-
-    app.listen(PORT , ()=> {
-
-console.log(`server is up and running on port ${PORT}`);
-
-});
+app.get("/api/health" , (req , res)=>{
+    res.status(200).json({status:"ok"});
 })
+
+app.use("/api/transactions", transactionsRoute);
+
+intDB().then(() => {
+    app.listen(PORT, () => {
+        console.log(`server is up and running on port ${PORT}`);
+    });
+}); 
